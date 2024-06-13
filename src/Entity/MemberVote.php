cfa56bd15dc7ffe5900ec761383917415337ab22
simbr <brown.simon.a@gmail.com>
2024-06-13 10:43:22 +0200

    first commit

---
+++ src/Entity/MemberVote.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table]
+class MemberVotes
+{
+    public const VOTE_FOR = 1;
+    public const VOTE_AGAINST = 2;
+    public const VOTE_ABSTENTION = 3;
+    public const VOTE_DID_NOT_VOTE = 4;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    private ?Member $member = null;
+
+    private ?Vote $vote = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMember(): ?Member
+    {
+        return $this->member;
+    }
+
+    public function setMember(?Member $member): self
+    {
+        $this->member = $member;
+
+        return $this;
+    }
+
+    public function getVote(): ?Vote
+    {
+        return $this->vote;
+    }
+
+    public function setVote(?Vote $vote): self
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(?int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
