8d494e5789a3b46612361457e462c199f17b5f08
simbr <brown.simon.a@gmail.com>
2024-09-11 19:55:10 +0200

    Update HP matrix data and style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,20 +24,45 @@ class HomeController extends AbstractController
     {
         $matrix = [];
         $memberBaseName = 'Member ';
-        foreach (range(1, 6) as $item) {
-            foreach (range(1, 6) as $item2) {
+        foreach (range(1, 10) as $item) {
+            foreach (range(1, 10) as $item2) {
                 if ($item === $item2) {
                     $matrix[$memberBaseName . $item][$memberBaseName . $item2] = [];
                     continue;
                 }
 
                 $matrix[$memberBaseName . $item][$memberBaseName . $item2] = [
-                    'rate' => rand(10, 100),
+                    'rate' => $this->fakeMatrixRate($item, $item2),
                     'nb_vote' => 100,
                 ];
             }
         }
 
         return $matrix;
+    }
+
+    private function fakeMatrixRate(int $m1, int $m2): int
+    {
+        if ($m1 <= 3 && $m2 <= 3) {
+            return rand(85, 100);
+        }
+
+        if ($m1 > 3 && $m1 <= 6 && $m2 > 3 && $m2 <= 6) {
+            return rand(85, 100);
+        }
+
+        if ($m1 <= 3 && $m2 <= 6) {
+            return rand(50, 70);
+        }
+
+        if ($m1 <= 6 && $m2 <= 3) {
+            return rand(50, 70);
+        }
+
+        if ($m1 > 6 && $m1 <= 9 && $m2 > 6 && $m2 <= 9) {
+            return rand(85, 100);
+        }
+
+        return rand(10, 80);
     }
 }
